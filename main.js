34f77bcb902b87ef71a9c7feaa261a26991fcc8f
CDebchi <45558761+CDebchi@users.noreply.github.com>
2019-01-29 11:14:10 +0100

    carosel + api

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+function afficher (k){
+  var ch = "<ul>";
+  for (var j = 0; j<k.length; j++){
+    ch += `<li>${k[j].label_service}</li>`
+  }
+  ch += "</ul>";
+  return ch;
+}
+$.getJSON("https://jsonp.afeld.me/?callback=?&url=https://api.myglamapp.pl/api/categories?language=EN", function (api) {
+console.log(api.data);
+
+for (i = 0; i<api.data.length; i++){   
+  var k =api.data[i].Services;
+  var l = afficher(k);  
+  var doc = document.getElementById(`div${i}`);
+    doc.innerHTML += `<div>
+    <img src="http://${api.data[i].imagePath}">      
+    <h5 class="text-center">${api.data[i].label}</h5>
+    ${l}        
+  </div>`;
+  
+    
+}
+});
 $(document).on('ready', function() {
     $(".regular").slick({
       dots: true,
@@ -6,3 +30,4 @@ $(document).on('ready', function() {
       slidesToScroll: 1
     });
   });
+  
